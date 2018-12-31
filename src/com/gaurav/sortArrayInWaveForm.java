df82208dc141d77aaceefe4f177ee991b640b144
Gaurav Kaushik <vgauravk@gmail.com>
2019-01-01 00:26:51 +0530

    Sort an array in wave form in O(nlogn)

---
+++ src/com/gaurav/sortArrayInWaveForm.java
@@ -0,0 +1,62 @@
+/*
+* Given an unsorted array of integers, sort the array into a wave like array. An array ‘arr[0..n-1]’ is
+* sorted in wave form if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
+Examples:
+
+ Input:  arr[] = {10, 5, 6, 3, 2, 20, 100, 80}
+ Output: arr[] = {10, 5, 6, 2, 20, 3, 100, 80} OR
+                 {20, 5, 10, 2, 80, 6, 100, 3} OR
+                 any other array that is in wave form
+
+ Input:  arr[] = {20, 10, 8, 6, 4, 2}
+ Output: arr[] = {20, 8, 10, 4, 6, 2} OR
+                 {10, 8, 20, 2, 6, 4} OR
+                 any other array that is in wave form
+
+ Input:  arr[] = {2, 4, 6, 8, 10, 20}
+ Output: arr[] = {4, 2, 8, 6, 20, 10} OR
+                 any other array that is in wave form
+
+ Input:  arr[] = {3, 6, 5, 10, 7, 20}
+ Output: arr[] = {6, 3, 10, 5, 20, 7} OR
+any other array that is in wave form
+
+A Simple Solution is to use sorting. First sort the input array, then swap all adjacent elements.
+
+For example, let the input array be {3, 6, 5, 10, 7, 20}. After sorting, we get {3, 5, 6, 7, 10, 20}. After swapping adjacent elements, we get {5, 3, 7, 6, 20, 10}.
+
+Below are implementations of this simple approach.
+*/
+
+package com.gaurav;
+
+import java.util.Arrays;
+
+public class sortArrayInWaveForm {
+    public static void main(String[] args) {
+        int[] array = {10, 90, 49, 2, 1, 5, 23};
+
+        sortArrayInWaveForm(array);
+
+        for (int i: array){
+            System.out.print(i + " ");
+        }
+    }
+
+    static void swap(int[] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static void sortArrayInWaveForm(int[] array){
+        int length = array.length;
+
+        Arrays.sort(array);
+
+        for (int i=0; i<array.length-1; i+=2){
+            swap(array, i, i+1);
+        }
+
+    }
+}
